90dfc99b49359bf5667b8bc7854e3a5d6898c9af
Faizan. A Rakin <rakinfaizanahmad@gmail.com>
2025-02-25 10:47:23 -0800

    Add pseudocode

---
+++ main.py
@@ -0,0 +1,19 @@
+# Create a program that will take user input about dinner guests they are inviting
+
+# Add Greeting
+
+# Ask the user how many guests they want to invite
+
+# Add names to a list
+
+# Create a loop that will keep appending names to the list until the maximum number is reached
+
+# Print out invitations for each dinner guest
+
+# Ask the user if they want to replace someone
+
+# Replace person if user chooses "y"
+
+# Regenerate invitation list
+
+# Add play again 
