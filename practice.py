bec6de2606ec90b5c6b8ca222606cee953b707b9
Faizan. A Rakin <rakinfaizanahmad@gmail.com>
2025-02-26 09:42:24 -0800

    Practice removing from list

--- practice.py
+++ practice.py
@@ -4,14 +4,16 @@ guest_list = []
 
 guests = 1
 
+counter = 1
+
 while True:
     
-    if guests <= 6:
+    if guests <= 2:
         guest_list.append(input("Invite a person to The Grand Banquet? : ").title().strip())
         guests += 1
         continue
 
-    elif guests > 6: 
+    elif guests > 2: 
         break
 
 # Make a list that includes these people and use the list to generate a personalized message to each person, inviting them to dinner.
@@ -23,3 +25,9 @@ print(f"You have invited {len(guest_list)} people to The Grand Banquet ")
 for guest in guest_list:
     print(f"Hi {guest}! You have graciously been invited to The Grand Banquet!")
 
+remove_guest = int(input("Please enter the number of the guest you want to replace: "))
+
+
+guest_list[remove_guest - 1] = input("Who do you want to replace the person with? : ").strip().capitalize()
+
+print(guest_list)
